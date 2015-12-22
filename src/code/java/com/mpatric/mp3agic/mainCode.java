5064447391133fc8f61bf45d586fb97c376bf06c
Amri <iliasamri00@gmail.com>
2015-12-22 13:37:49 +0100

    Mise en place du projet

---
+++ src/code/java/com/mpatric/mp3agic/mainCode.java
@@ -0,0 +1,13 @@
+package com.mpatric.mp3agic;
+
+import com.mpatric.mp3agic.*;
+
+public class mainCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+	}
+
+}
