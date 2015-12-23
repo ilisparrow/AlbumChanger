e10c845839fc56053a352a81b00c54a73db4846b
Amri <iliasamri00@gmail.com>
2015-12-23 21:23:32 +0100

    Final 1

--- src/code/java/com/mpatric/mp3agic/mainCode.java
+++ src/code/java/com/mpatric/mp3agic/mainCode.java
@@ -1,54 +1,81 @@
 package com.mpatric.mp3agic;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class mainCode {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		File curDir = new File(".");
-		int NumberOfFiles = nbrefichier(curDir);
-		String[] list;
-		Mp3File mp3[] = new Mp3File[nbrefichier(curDir)];
+		final int NumberOfFiles = nbrefichier(curDir);
+		final String[] list;
+		final Mp3File mp3[] = new Mp3File[nbrefichier(curDir)];
 		// mp3[] = new Mp3File("un.mp3");
 		// System.out.println(mp3.getLengthInSeconds());
 
 		list = fileLoader(curDir);
-		for (int i = 0; i < NumberOfFiles; i++) {
+		JFrame frame = new JFrame("Album Changer");
+		JPanel panel = new JPanel();
+		JButton btn = new JButton("Valider");
+		final JTextField txtField = new JTextField("Cocktail");
 
-			if (list[i].charAt(list[i].length() - 1) == '3') {
-				System.out.println(list[i]);
-				try {
-					mp3[i] = new Mp3File(list[i]);
-					System.out.println(mp3[i].getLengthInSeconds());
-					ID3v1 id3v1Tag;
-					
-					if (mp3[i].hasId3v1Tag()) {
-						id3v1Tag = mp3[i].getId3v1Tag();
-					}
-					else{
-						id3v1Tag = new ID3v1Tag();
-						mp3[i].setId3v1Tag(id3v1Tag);
-					}
-					
-					id3v1Tag.setAlbum("Hello_World");
-					mp3[i].setId3v1Tag(id3v1Tag);
-					try {
-						mp3[i].save("1"+list[i]);
-					} catch (NotSupportedException e) {
-						e.printStackTrace();
+		panel.add(txtField);
+		panel.add(btn);
+
+		frame.setSize(250, 70);
+		frame.setVisible(true);
+		frame.setAlwaysOnTop(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+
+		// Evenement
+		/*btn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {*/
+				for (int i = 0; i < NumberOfFiles; i++) {
+
+					if (list[i].charAt(list[i].length() - 1) == '3') {
+						System.out.println(list[i]);
+						try {
+							mp3[i] = new Mp3File(list[i]);
+							// System.out.println(mp3[i].getLengthInSeconds());
+							ID3v1 id3v1Tag;
+
+							if (mp3[i].hasId3v1Tag()) {
+								id3v1Tag = mp3[i].getId3v1Tag();
+							} else {
+								id3v1Tag = new ID3v1Tag();
+								mp3[i].setId3v1Tag(id3v1Tag);
+							}
+
+							id3v1Tag.setAlbum(txtField.getText());
+							mp3[i].setId3v1Tag(id3v1Tag);
+							try {
+								mp3[i].save("_" + list[i]);
+							} catch (NotSupportedException e1) {
+								e1.printStackTrace();
+							}
+
+						} catch (UnsupportedTagException | InvalidDataException | IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
 					}
 
-				} catch (UnsupportedTagException | InvalidDataException | IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
 				}
 			}
+		//});
 
-		}
-
-	}
+	//}
 
 	private static String[] fileLoader(File _curDir) {// Returns a string of all
 														// files in the main
