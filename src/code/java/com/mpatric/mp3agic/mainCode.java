8b593350aab8eaf444dcb57a52e8f9aae6c1761c
Amri <iliasamri00@gmail.com>
2015-12-23 17:01:32 +0100

    Generalisation de la recherche de fichier mp3

--- src/code/java/com/mpatric/mp3agic/mainCode.java
+++ src/code/java/com/mpatric/mp3agic/mainCode.java
@@ -1,13 +1,58 @@
 package com.mpatric.mp3agic;
 
-import com.mpatric.mp3agic.*;
+import java.io.File;
+import java.io.IOException;
 
 public class mainCode {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Mp3File mp3[];
+		File curDir = new File(".");
+		String[] list;
+		int i=0;
+		/*try {
+			//mp3[] = new Mp3File("un.mp3");
+			//System.out.println(mp3.getLengthInSeconds());
+		} catch (UnsupportedTagException | InvalidDataException | IOException e) {*/
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		list = fileLoader(curDir);
+		for (String s : list) {
+			if (s.charAt(s.length()-1)=='3') {
+				System.out.println(s);
+			}
 
-	
+		}
+		
+		
+		
 	}
 
+	private static String[] fileLoader(File _curDir) {//Returns a string of all files in the main repesitory
+		File[] files = _curDir.listFiles();
+		String[] listeNomFichiers = new String[files.length];
+		int i = 0;
+
+		for (File f : files) {
+			if (f.isDirectory()) {
+				/* System.out.println(f.getName()); */} 
+			else if (f.isFile()) {
+				// System.out.println(f.getName());
+					
+					listeNomFichiers[i]= f.getName();
+					
+				i++;
+			}
+			
+		}
+		return listeNomFichiers;
+
+	}
+	private static int nbrefichier(File _curDir) {//Returns a string of all files in the main repesitory
+		File[] files = _curDir.listFiles();
+		return files.length;
+
+	}
 }
