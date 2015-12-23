c555bf7a466dc40327295203163bf23fdd18593b
Amri <iliasamri00@gmail.com>
2015-12-23 18:28:03 +0100

    AJout des tag et modification, fonctionne parfaitement

--- src/code/java/com/mpatric/mp3agic/mainCode.java
+++ src/code/java/com/mpatric/mp3agic/mainCode.java
@@ -7,50 +7,74 @@ public class mainCode {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Mp3File mp3[];
 		File curDir = new File(".");
+		int NumberOfFiles = nbrefichier(curDir);
 		String[] list;
-		int i=0;
-		/*try {
-			//mp3[] = new Mp3File("un.mp3");
-			//System.out.println(mp3.getLengthInSeconds());
-		} catch (UnsupportedTagException | InvalidDataException | IOException e) {*/
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		Mp3File mp3[] = new Mp3File[nbrefichier(curDir)];
+		// mp3[] = new Mp3File("un.mp3");
+		// System.out.println(mp3.getLengthInSeconds());
+
 		list = fileLoader(curDir);
-		for (String s : list) {
-			if (s.charAt(s.length()-1)=='3') {
-				System.out.println(s);
+		for (int i = 0; i < NumberOfFiles; i++) {
+
+			if (list[i].charAt(list[i].length() - 1) == '3') {
+				System.out.println(list[i]);
+				try {
+					mp3[i] = new Mp3File(list[i]);
+					System.out.println(mp3[i].getLengthInSeconds());
+					ID3v1 id3v1Tag;
+					
+					if (mp3[i].hasId3v1Tag()) {
+						id3v1Tag = mp3[i].getId3v1Tag();
+					}
+					else{
+						id3v1Tag = new ID3v1Tag();
+						mp3[i].setId3v1Tag(id3v1Tag);
+					}
+					
+					id3v1Tag.setAlbum("Hello_World");
+					mp3[i].setId3v1Tag(id3v1Tag);
+					try {
+						mp3[i].save("1"+list[i]);
+					} catch (NotSupportedException e) {
+						e.printStackTrace();
+					}
+
+				} catch (UnsupportedTagException | InvalidDataException | IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 
 		}
-		
-		
-		
+
 	}
 
-	private static String[] fileLoader(File _curDir) {//Returns a string of all files in the main repesitory
+	private static String[] fileLoader(File _curDir) {// Returns a string of all
+														// files in the main
+														// repesitory
 		File[] files = _curDir.listFiles();
 		String[] listeNomFichiers = new String[files.length];
 		int i = 0;
 
 		for (File f : files) {
 			if (f.isDirectory()) {
-				/* System.out.println(f.getName()); */} 
-			else if (f.isFile()) {
+				/* System.out.println(f.getName()); */} else if (f.isFile()) {
 				// System.out.println(f.getName());
-					
-					listeNomFichiers[i]= f.getName();
-					
+
+				listeNomFichiers[i] = f.getName();
+
 				i++;
 			}
-			
+
 		}
 		return listeNomFichiers;
 
 	}
-	private static int nbrefichier(File _curDir) {//Returns a string of all files in the main repesitory
+
+	private static int nbrefichier(File _curDir) {// Returns a string of number
+													// of files in the main
+													// repesitory
 		File[] files = _curDir.listFiles();
 		return files.length;
 
